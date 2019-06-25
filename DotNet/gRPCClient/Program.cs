4eb139fc853c7968545857ecac7ec5f9c5a6222c
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2019-06-25 02:22:03 -0300

    Baseline

---
+++ DotNet/gRPCClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using System;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+
+
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+
+            var client = new gRPCHost.Greeter.GreeterClient(channel);
+
+
+            var response = client.SayHello(new gRPCHost.HelloRequest() { Name = "gaGO.io" });
+
+
+            Console.WriteLine("Hello World gRPC! " + response.Message);
+
+
+            await channel.ShutdownAsync();
+
+        }
+    }
+}
